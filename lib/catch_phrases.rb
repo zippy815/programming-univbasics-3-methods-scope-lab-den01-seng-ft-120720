d20902512ebb0373148ee86919e8812a96a17f10
Zipporah Horch <zipporahhorch@gmail.com>
2020-10-28 19:30:54 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,9 +2,9 @@ $phrase = "It's-a me, Mario!"
 def mario
   puts $phrase
 end
-
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
   puts status
 end
 def link 
